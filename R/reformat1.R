8f9c11e13582b497f6a515a348f75fce98a0fcbd
hycheon123 <jeon@iastate.edu>
2017-06-05 15:35:07 -0500

    update

--- R/reformat1.R
+++ R/reformat1.R
@@ -7,7 +7,8 @@
 #' @examples
 #' setwd("C:/Users/kbs/Desktop/RA_Pig")
 #' # data = original data in .csv format 
-#' data<-read.csv("LI-A-2.csv")
+#' data<-read.csv("HN-A-2.csv")
+#' # result will be "HN-A.csv"
 
 reformat1<-function(data,k){
   # data = original data
@@ -19,10 +20,9 @@ reformat1<-function(data,k){
   temp<-data.frame(matrix(NA,nrow=N,ncol=(k+3)))
   colnames(temp)<-c("Group","Names",paste0("obs",c(1:k)),"Ions score")
   for(i in 1:N){
-    if(as.logical(1-is.na(data2$Checked[i]))){temp[i,]<-c("Protein",as.character(data2[i,3]),as.numeric(as.matrix(data2[i,c(30:(30+k-1))])),NA)}
-    if(is.na(data2$Checked[i])){temp[i,]<-c("Peptide",as.character(data2[i,4]),as.numeric(as.matrix(data2[i,c(22:(22+k-1))])))}
+    #Be careful about the column name.
+    if(as.logical(1-is.na(data2$Checked[i]))){temp[i,]<-c("Protein",as.character(data2[i,3]),as.numeric(as.matrix(data2[i,c((14+2*k):(13+3*k))])),NA)}
+    if(is.na(data2$Checked[i])){temp[i,]<-c("Peptide",as.character(data2[i,4]),as.numeric(as.matrix(data2[i,c((14+k):(14+2*k))])))}
   }
   return(temp)
 }
-
-
